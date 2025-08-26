a31ef4bb6ec70efab19a1cd65cce2b86457800fe
hanna09bravo-collab <hanna09bravo@gmail.com>
2025-08-25 19:14:44 -0500

    programaa3.py

--- programaa3.py
+++ programaa3.py
@@ -1,6 +1,6 @@
 import streamlit as st
 import pandas as pd
 import numpy as np
-df = pd.DataFrame(
-np.random.randn(1000, 2) / [50, 50] + [37.76, -122.4],
-columns=['lat', 'lon'])
+n = st.slider("n", 5,100, step=1)
+chart_data = pd.DataFrame(np.random.randn(n),columns=['data'])
+st.line_chart(chart_data)
