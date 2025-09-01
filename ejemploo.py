b57524d145708335312c29e36785245fd70fd25f
hanna09bravo-collab <hanna09bravo@gmail.com>
2025-09-01 16:55:38 -0500

    ejemploo.py

--- ejemploo.py
+++ ejemploo.py
@@ -13,3 +13,20 @@ appointment = st.slider(
 "Programe la asesoria:",
 value=(time(11, 30), time(12, 45)))
 st.write("Esta agendado para:", appointment)
+
+
+import streamlit as st
+import pandas as pd
+import numpy as np
+import datetime
+d = st.date_input(
+"Fecha de cumpleaños",
+datetime.date(2019, 7, 6))
+st.write('Tu cumpleños es:', d)
+
+
+import streamlit as st
+option = st.selectbox(
+'¿Cómo desearía ser contactado/a?',
+('Email', 'Teléfono', 'Whatsapp'))
+st.write('Seleccionó:', option)
